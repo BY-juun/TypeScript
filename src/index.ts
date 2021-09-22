bee3d8f7d5acc0a6cfecaeb9bc0cee2d0b21e444
ByeongJun <neostgeart@ajou.ac.kr>
2021-09-22 21:36:29 +0900

    Interface Class

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,36 @@
+import * as CryptoJS from 'crypto-js';
 
-const sayHi = (name:string,age:number,gender:string): string => {
-    return (`Hello ${name}, you are ${age}, you are a ${gender}!!`);
-};
+class Block{
+    public index : number;
+    public hash : string;
+    public previousHash : string;
+    public data : string;
+    public timestamp : number;
 
-console.log(sayHi('By_Juun',24,'male'));
+    static calculateBlockHash = (index:number,previousHash:string,timestamp:number,data:string) : string => 
+    CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+
+
+    constructor(index : number,hash : string,previousHash : string,data : string,timestamp : number){
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timestamp = timestamp;
+    }
+}
+
+
+const genesisBlock : Block = new Block(0,"20202020202020","","Hello",123456);
+
+let blockChain : Block[] = [genesisBlock];
+
+const getBlockchain = () : Block[] => blockChain;
+
+const getLastestBlock = () : Block => blockChain[blockChain.length - 1];
+
+const getNewTimeStamp = () : number => Math.round(new Date().getTime() / 1000);
+
+console.log(blockChain);
 
 export{}
